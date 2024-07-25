a9dacf6d2f46e44419f1123a37ec1ecaeed24c09
DevTimlas <mac@Tims-MacBook-Pro.local>
2024-07-25 13:35:49 +0100

    added more features

--- .ipynb_checkpoints/main_text-checkpoint.py
+++ .ipynb_checkpoints/main_text-checkpoint.py
@@ -17,6 +17,7 @@ app = Flask(__name__)
 
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_groq import ChatGroq
+from langchain_openai import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import LLMChain
 from langchain_core.prompts.chat import (
@@ -27,9 +28,13 @@ from langchain_core.prompts.chat import (
 )
 import json
 
+tokenizer = AutoTokenizer.from_pretrained('cross-encoder/ms-marco-TinyBERT-L-2-v2', cache_dir='./cache_L')
+model = AutoModel.from_pretrained('cross-encoder/ms-marco-TinyBERT-L-2-v2', cache_dir='./cache_L')
+
+
 def get_q(msg):
-    llm = ChatGroq(temperature=0, model="llama3-70b-8192",
-                       api_key="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")
+    llm = ChatOpenAI(temperature=0, model='gpt-4-turbo-2024-04-09',
+                       api_key="sk-proj-pHkywA2mDUiHGcCWRM4TT3BlbkFJz3qlbeGgfG8C70iHX7WK")
     memory = ConversationBufferMemory(memory_key="get_bed", return_messages=True)
     sys_prompt = f"""The user says: "{msg}", Just incase the text is long and not straightforward, Look for the main 
     information in the user's input and extract it. main information is the number of bedrooms, so, anything the user type
@@ -43,7 +48,12 @@ def get_q(msg):
     if any of the key parameter is passed, just automatically pass none and map it to the key that the value is not provided
     for example the user might not mention of they're looking for a house that's for rent or for sale.. in that case, just pass the
     unknown keyword to the sub_type, and if they didn't mention number of bedroom they want, just pass unknown keyword to bedroom
-    likewise for location too
+    likewise for location too.
+    extract the price and map it to price key, map the price key to unknown if it's not found in the sentence
+        prices can be a number in full like 1000, 10,000, 1,000,000 or a short form such as 1K, 10K, 1M, 1B, etc 
+        Take note of price modifiers such as less than, more than, for etc...
+    extract the agent name and map it to agent key, map the agent key to unknown if it's not found in the sentence
+    extract the property type and map it to property, map the property key to unknown if it's not found in the sentence
     """
     prompt = ChatPromptTemplate.from_messages([SystemMessagePromptTemplate.from_template(sys_prompt),
                                                MessagesPlaceholder(variable_name="get_bed"),
@@ -81,44 +91,78 @@ def embed_text(text, tokenizer, model):
         outputs = model(**inputs)
     return outputs.last_hidden_state.mean(dim=1).squeeze().numpy()
 
-# Function to calculate Inner Product from L2 score
 def calculateInnerProduct(L2_score):
     import math
-    return (2 - math.pow(L2_score, 2)) / 2
+    return (2-math.pow(L2_score,2))/2
 
-# Function to search FAISS index
+
 def searchFAISSIndex(data, query, index, nprobe, tokenizer, model, topk=20):
+    # Convert the query into embeddings for description, location, and bedroom
     description_embedding = embed_text(query['query'], tokenizer, model)
     location_embedding = embed_text(query['location'], tokenizer, model)
     bedroom_embedding = embed_text(query['bedroom'], tokenizer, model)
-    submission_type_embedding = embed_text(query['sub_type'], tokenizer, model)
+    sub_type_embedding = embed_text(query['sub_type'], tokenizer, model)
+    prop_embedding = embed_text(query['property'], tokenizer, model)
+    price_embedding = embed_text(query['price'], tokenizer, model)
+    agent_embedding = embed_text(query['agent'], tokenizer, model)
     
-    query_embedding = np.hstack([description_embedding, location_embedding, bedroom_embedding, submission_type_embedding])
-    query_embedding = query_embedding.reshape(1, -1)
+    # Combine the embeddings
+    query_embedding = np.hstack([description_embedding, location_embedding, bedroom_embedding, 
+                                 sub_type_embedding, prop_embedding, price_embedding, agent_embedding])
+    
+    # Normalize the combined embedding
+    dim = query_embedding.shape[0]
+    query_embedding = query_embedding.reshape(1, dim)
     faiss.normalize_L2(query_embedding)
     
+    # Set the number of probes
     index.nprobe = nprobe
-    D, I = index.search(query_embedding, topk)
     
+    # Search the index
+    D, I = index.search(query_embedding, topk)
+    print("Distances:", D)
+    print("Indices:", I)
+    
+    # Extract the indices and scores
     indices = I[0]
     L2_score = D[0]
     inner_product = [calculateInnerProduct(l2) for l2 in L2_score]
+    # print("Inner products:", inner_product)
+    # print("FAISS indices:", indices)
     
+    # Retrieve rows using iloc
     matching_data = data.iloc[indices]
+    # print("Matching data using iloc:", matching_data)
     
+    # Create a DataFrame with the search results
     search_result = pd.DataFrame({
         'index': indices,
         'cosine_sim': inner_product,
         'L2_score': L2_score
     })
+    # print("Search result DataFrame:", search_result)
     
-    dat = pd.concat([matching_data.reset_index(drop=True), search_result.reset_index(drop=True)], axis=1)
-    dat = dat.sort_values('cosine_sim', ascending=False)
+    # Merge the search results with the original data
+    dat = pd.concat([matching_data.reset_index(drop=True), search_result.reset_index(drop=True)], axis=1)    
+    dat = dat.sort_values(by=['likes', 'views'], ascending=False)
+    dat = dat.sort_values(by=['price.price'], ascending=True)
+    dat[f"is_{query['location'].lower()}"] = dat['location'].str.lower() == query['location'].lower()
+    dat[f"is_{query['sub_type'].lower()}"] = dat['submission_type'].str.lower() == query['sub_type'].lower()
+    dat[f"is_{query['bedroom'].lower()}"] = dat['bedrooms'].str.contains(query['bedroom'], case=False, na=False)
+
+    dat['bedrooms_numeric'] = dat['bedrooms'].str.extract('(\d+)', expand=False).astype(float)
+
+    # Sort by is_lavington first, then by is_rent, then by location, and finally by submission type
+    dat = dat.sort_values(by=[f"is_{query['location'].lower()}", 'is_rent', f"is_{query['bedroom'].lower()}", 'location', 'submission_type', 'bedrooms_numeric'], 
+                          ascending=[False, False, False, True, True, True])
+
     
     return dat
 
+
 @app.route('/search', methods=['POST'])
 def search():
+    global tokenizer, model
     inp = request.json
     # print(inp)
     query = get_q(inp.get("query"))
@@ -144,35 +188,41 @@ def search():
         return jsonify({"error": "No data was fetched from the API."})
 
     properties['description'] = properties['description'].str.lower()
+    properties['location'] = properties['location'].str.lower()
+    properties['price.price'] = properties['price.price'].astype(str)
     properties['description'] = properties['description'].apply(clean_text)
     properties = properties.dropna(axis=1, how='all')
     properties = properties.dropna(subset=['location', 'bedrooms', 'description'])
-
-    tokenizer = AutoTokenizer.from_pretrained('cross-encoder/ms-marco-TinyBERT-L-2-v2', cache_dir='./cache_L')
-    model = AutoModel.from_pretrained('cross-encoder/ms-marco-TinyBERT-L-2-v2', cache_dir='./cache_L')
 
     properties['description_embedding'] = properties['description'].apply(lambda x: embed_text(x, tokenizer, model))
     properties['location_embedding'] = properties['location'].apply(lambda x: embed_text(x, tokenizer, model))
     properties['bedroom_embedding'] = properties['bedrooms'].apply(lambda x: embed_text(x, tokenizer, model))
     properties['submission_type_embedding'] = properties['submission_type'].apply(lambda x: embed_text(x, tokenizer, model))
-    
+    properties['property_type_embedding'] = properties['property_type'].apply(lambda x: embed_text(x, tokenizer, model))
+    properties['price_embedding'] = properties['price.price'].apply(lambda x: embed_text(x, tokenizer, model))
+    properties['agent_embedding'] = properties['real_agent.name'].apply(lambda x: embed_text(x, tokenizer, model))
 
     combined_features = np.hstack([
         properties['description_embedding'].tolist(),
         properties['location_embedding'].tolist(),
         properties['bedroom_embedding'].tolist(),
-        properties['submission_type_embedding'].tolist()
+        properties['submission_type_embedding'].tolist(),
+        properties['property_type_embedding'].tolist(),
+        properties['price_embedding'].tolist(),
+        properties['agent_embedding'].tolist(),
     ])
 
     X = normalize(combined_features.astype('float32'))
-    d = X.shape[1]
+    d = X.shape[1] 
     index = faiss.IndexFlatL2(d)
     index.add(X)
 
     search_result = searchFAISSIndex(properties, query, index, nprobe=5, tokenizer=tokenizer, model=model, topk=topk)
+    search_result = search_result.drop(['description_embedding', 'location_embedding', 'bedroom_embedding', 'submission_type_embedding', 'property_type_embedding', 'price_embedding', 'agent_embedding'], axis=1)
+
 
     if not search_result.empty:
-        search_result = search_result[['index', 'description', 'location', 'bedrooms', 'price.price', 'cosine_sim', 'L2_score']]
+        # search_result = search_result[['index', 'description', 'location', 'bedrooms', 'price.price', 'cosine_sim', 'L2_score']]
         return search_result.to_json(orient='records')
     else:
         return jsonify({"error": "No matching results found."})
--- main_last.py
+++ main_last.py
@@ -17,6 +17,7 @@ app = Flask(__name__)
 
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_groq import ChatGroq
+from langchain_openai import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import LLMChain
 from langchain_core.prompts.chat import (
@@ -27,14 +28,14 @@ from langchain_core.prompts.chat import (
 )
 import json
 
-index = faiss.read_index('real_estate_final_index.faiss')
-properties = pd.read_csv('modified_properties.csv')
+index = faiss.read_index('real_estate_final_index_new.faiss')
+properties = pd.read_csv('modified_properties_new.csv')
 tokenizer = AutoTokenizer.from_pretrained('cross-encoder/ms-marco-TinyBERT-L-2-v2', cache_dir='./cache_L')
 model = AutoModel.from_pretrained('cross-encoder/ms-marco-TinyBERT-L-2-v2', cache_dir='./cache_L')
 
 def get_q(msg):
-    llm = ChatGroq(temperature=0, model="llama3-70b-8192",
-                       api_key="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")
+    llm = ChatOpenAI(temperature=0, model='gpt-4-turbo-2024-04-09',
+                       api_key="sk-proj-pHkywA2mDUiHGcCWRM4TT3BlbkFJz3qlbeGgfG8C70iHX7WK")
     memory = ConversationBufferMemory(memory_key="get_bed", return_messages=True)
     sys_prompt = f"""The user says: "{msg}", Just incase the text is long and not straightforward, Look for the main 
     information in the user's input and extract it. main information is the number of bedrooms, so, anything the user type
@@ -48,7 +49,12 @@ def get_q(msg):
     if any of the key parameter is passed, just automatically pass none and map it to the key that the value is not provided
     for example the user might not mention of they're looking for a house that's for rent or for sale.. in that case, just pass the
     unknown keyword to the sub_type, and if they didn't mention number of bedroom they want, just pass unknown keyword to bedroom
-    likewise for location too
+    likewise for location too.
+    extract the price and map it to price key, map the price key to unknown if it's not found in the sentence
+        prices can be a number in full like 1000, 10,000, 1,000,000 or a short form such as 1K, 10K, 1M, 1B, etc 
+        Take note of price modifiers such as less than, more than, for etc...
+    extract the agent name and map it to agent key, map the agent key to unknown if it's not found in the sentence
+    extract the property type and map it to property, map the property key to unknown if it's not found in the sentence
     """
     prompt = ChatPromptTemplate.from_messages([SystemMessagePromptTemplate.from_template(sys_prompt),
                                                MessagesPlaceholder(variable_name="get_bed"),
@@ -57,10 +63,16 @@ def get_q(msg):
 
     memory.chat_memory.add_user_message(msg)
     response = conversation.invoke({"text": msg})
-    # print("entity", response['text'])
-    # print(json.loads(response['text']))
     return json.loads(response['text'])
 
+
+# Function to clean HTML tags from string
+def clean_text(raw_html):
+    clean_text = BeautifulSoup(raw_html, "html.parser").get_text()
+    clean_text = clean_text.replace('📌', '').strip()
+    clean_text = emoji.replace_emoji(clean_text, replace='')
+    return clean_text
+    
 
 # Function to embed text
 def embed_text(text, tokenizer, model):
@@ -75,34 +87,66 @@ def calculateInnerProduct(L2_score):
     import math
     return (2 - math.pow(L2_score, 2)) / 2
 
-# Function to search FAISS index
 def searchFAISSIndex(data, query, index, nprobe, tokenizer, model, topk=20):
+    # Convert the query into embeddings for description, location, and bedroom
     description_embedding = embed_text(query['query'], tokenizer, model)
     location_embedding = embed_text(query['location'], tokenizer, model)
     bedroom_embedding = embed_text(query['bedroom'], tokenizer, model)
-    submission_type_embedding = embed_text(query['sub_type'], tokenizer, model)
+    sub_type_embedding = embed_text(query['sub_type'], tokenizer, model)
+    prop_embedding = embed_text(query['property'], tokenizer, model)
+    price_embedding = embed_text(query['price'], tokenizer, model)
+    agent_embedding = embed_text(query['agent'], tokenizer, model)
     
-    query_embedding = np.hstack([description_embedding, location_embedding, bedroom_embedding, submission_type_embedding])
-    query_embedding = query_embedding.reshape(1, -1)
+    # Combine the embeddings
+    query_embedding = np.hstack([description_embedding, location_embedding, bedroom_embedding, 
+                                 sub_type_embedding, prop_embedding, price_embedding, agent_embedding])
+    
+    # Normalize the combined embedding
+    dim = query_embedding.shape[0]
+    query_embedding = query_embedding.reshape(1, dim)
     faiss.normalize_L2(query_embedding)
     
+    # Set the number of probes
     index.nprobe = nprobe
-    D, I = index.search(query_embedding, topk)
     
+    # Search the index
+    D, I = index.search(query_embedding, topk)
+    print("Distances:", D)
+    print("Indices:", I)
+    
+    # Extract the indices and scores
     indices = I[0]
     L2_score = D[0]
     inner_product = [calculateInnerProduct(l2) for l2 in L2_score]
+    # print("Inner products:", inner_product)
+    # print("FAISS indices:", indices)
     
+    # Retrieve rows using iloc
     matching_data = data.iloc[indices]
+    # print("Matching data using iloc:", matching_data)
     
+    # Create a DataFrame with the search results
     search_result = pd.DataFrame({
         'index': indices,
         'cosine_sim': inner_product,
         'L2_score': L2_score
     })
+    # print("Search result DataFrame:", search_result)
     
-    dat = pd.concat([matching_data.reset_index(drop=True), search_result.reset_index(drop=True)], axis=1)
-    dat = dat.sort_values('cosine_sim', ascending=False)
+    # Merge the search results with the original data
+    dat = pd.concat([matching_data.reset_index(drop=True), search_result.reset_index(drop=True)], axis=1)    
+    dat = dat.sort_values(by=['likes', 'views'], ascending=False)
+    dat = dat.sort_values(by=['price.price'], ascending=True)
+    dat[f"is_{query['location'].lower()}"] = dat['location'].str.lower() == query['location'].lower()
+    dat[f"is_{query['sub_type'].lower()}"] = dat['submission_type'].str.lower() == query['sub_type'].lower()
+    dat[f"is_{query['bedroom'].lower()}"] = dat['bedrooms'].str.contains(query['bedroom'], case=False, na=False)
+
+    dat['bedrooms_numeric'] = dat['bedrooms'].str.extract('(\d+)', expand=False).astype(float)
+
+    # Sort by is_lavington first, then by is_rent, then by location, and finally by submission type
+    dat = dat.sort_values(by=[f"is_{query['location'].lower()}", 'is_rent', f"is_{query['bedroom'].lower()}", 'location', 'submission_type', 'bedrooms_numeric'], 
+                          ascending=[False, False, False, True, True, True])
+
     
     return dat
 
@@ -116,10 +160,10 @@ def search():
     topk = inp.get('k', 20)  # Get 'k' from the request, default to 20 if not provided
 
     search_result = searchFAISSIndex(properties, query, index, nprobe=5, tokenizer=tokenizer, model=model, topk=topk)
-    search_result = search_result.drop(['description_embedding', 'location_embedding', 'bedroom_embedding', 'submission_type_embedding'], axis=1)
+    search_result = search_result.drop(['description_embedding', 'location_embedding', 'bedroom_embedding', 'submission_type_embedding', 'property_type_embedding', 'price_embedding', 'agent_embedding'], axis=1)
 
     if not search_result.empty:
-        # search_result = search_result[['index', 'description', 'location', 'bedrooms', 'price.price', 'cosine_sim', 'L2_score']]
+        # search_result = search_result[['index', 'description', 'location', 'bedrooms', 'price.price']]
         return search_result.to_json(orient='records')
     else:
         return jsonify({"error": "No matching results found."})
